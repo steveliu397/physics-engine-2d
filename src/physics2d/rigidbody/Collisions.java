571dbd2b6cf0283188d3e3554372e8bb63e2cb34
Steve Liu <steveliu397@Steves-MacBook-Air.local>
2024-02-16 14:28:50 -0800

    further implemented collisions section

--- src/physics2d/rigidbody/Collisions.java
+++ src/physics2d/rigidbody/Collisions.java
@@ -2,16 +2,29 @@ package physics2d.rigidbody;
 
 import org.joml.Vector2f;
 import physics2d.primitives.Circle;
+import physics2d.primitives.Collider2D;
 
 import java.util.Vector;
 
 public class Collisions {
+    public static CollisionManifold findCollisionFeatures(Collider2D c1, Collider2D c2) {
+        if (c1 instanceof Circle && c2 instanceof Circle) {
+            return findCollisionFeatures((Circle)c1, (Circle)c2);
+        }
+        else {
+            assert false : "Unknown collider '" + c1.getClass() + "' vs '" + c2.getClass() + "'";
+        }
+
+        return null;
+    }
+
     public static CollisionManifold findCollisionFeatures(Circle a, Circle b) {
         CollisionManifold result = new CollisionManifold();
         float sumRadii = a.getRadius() + b.getRadius();
         Vector2f distance = new Vector2f(a.getCenter()).sub(b.getCenter());
 
-        if(distance.lengthSquared() - Math.pow(sumRadii, 2) > 0) {
+        if(distance.lengthSquared() - Math.pow(sumRadii, 2) > 0)
+        {
             return result;
         }
 
